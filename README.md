ee0422789cfb416f98c38471bda79e0fa9146b71
nduhiu17 <nduhiu254@gmail.com>
2019-04-19 15:32:55 +0300

    add read me

---
+++ README.md
@@ -0,0 +1,3 @@
+# prime-number-checker
+
+A simple java program that checks whether a number is a prime number or not
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,28 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-    int i,m=0,flag=0;
-    int n=4;//it is the number to be checked
-    m=n/2;
-    if(n==0||n==1){
-        System.out.println(n+" is not a prime number");
-    }else{
-        for(i=2;i<=m;i++){
-            if(n%i==0){
-                System.out.println(n+" is not a prime number");
-                flag=1;
-                break;
-            }
-        }
-        if(flag==0){
-            System.out.println(n+ " is prime number");
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = s.nextInt();
+        if (isPrime(n)) {
+            System.out.println(n + " is a prime number");
+        } else {
+            System.out.println(n + " is not a prime number");
         }
     }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
